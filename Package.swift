9e7ef36c7658828cf204e76177c846035ad8f8ef
Abhishek Suryawanshi <iosdev.abhi@gmail.com>
2024-11-13 17:45:23 +0530

    Updated Package.swift file

--- Package.swift
+++ Package.swift
@@ -5,17 +5,25 @@ import PackageDescription
 
 let package = Package(
     name: "AbNetworkingPackage",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15),
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "AbNetworkingPackage",
             targets: ["AbNetworkingPackage"]),
     ],
+    dependencies: [
+        // No dependencies listed
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "AbNetworkingPackage"),
+            name: "AbNetworkingPackage",
+            dependencies: []),
         .testTarget(
             name: "AbNetworkingPackageTests",
             dependencies: ["AbNetworkingPackage"]
